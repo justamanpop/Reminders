bbb2999685ac6e4be5f03695ffa790966633e4a5
Anish Sekhar <pogchampoo24@gmail.com>
2021-04-15 12:59:31 +0530

    created alarm model

---
+++ Reminders/Controllers/AlarmController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Reminders.Controllers
+{
+    public class AlarmController:Controller
+    {
+
+    }
+}
