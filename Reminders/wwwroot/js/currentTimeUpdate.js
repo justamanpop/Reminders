f3b45dd331d1a2cb5cc13f77cf316dbeabad0faf
Anish Sekhar <pogchampoo24@gmail.com>
2021-04-28 11:42:30 +0530

    added javascript logic that iterates through each alarm and notifies user with what the text of alarm

---
+++ Reminders/wwwroot/js/currentTimeUpdate.js
@@ -0,0 +1,7 @@
+﻿function updateTime() {
+    var d = new Date().toLocaleTimeString();
+    var ele = document.getElementById("myTime");
+    ele.innerHTML = `The time is ${d}`;
+}
+
+setInterval(updateTime, 100);
---
+++ Reminders/wwwroot/js/notifications.js
@@ -0,0 +1,35 @@
+﻿function showNotification() {
+
+    let $descriptions = $(".card-text");
+
+    $('.card-title').each(function (index) { 
+
+        const notification = new Notification("Alarm", {
+            body: $($descriptions[index]).text()
+        });
+    });
+}
+
+console.log(Notification.permission);
+
+$('document').ready(() => {
+    console.log("in doc ready");
+    $('#notifyBtn').click(async () => {
+        console.log("I was reached")
+        if (Notification.permission === "granted") {
+            $('#notifyBtn').removeClass("btn-primary");
+            $('#notifyBtn').addClass("btn-secondary");
+            $('#notifyBtn').attr("disabled", true);
+            $('#notifyBtn').text("Notifications enabled!")
+
+            setInterval(showNotification, 2000);
+        }
+
+        else if (Notification.permission !== "denied") {
+            Notification.requestPermission().then(permission => {
+                showNotification();
+            })
+            showNotification();
+        }
+    });
+});
